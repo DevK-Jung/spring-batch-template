c63ad9dc3923cf7c5751ca4d30c7d340c5ebd9d9
김정현 <wjdgus0117@ccmedia.co.kr>
2025-06-04 17:40:28 +0900

    [Feat]: test Job 구현

---
+++ src/main/java/com/kjung/batchtemplate/jobs/sample/SampleJob.java
@@ -0,0 +1,39 @@
+package com.kjung.batchtemplate.jobs.sample;
+
+import com.kjung.batchtemplate.core.config.AbstractJobConfig;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.stream.IntStream;
+
+@Configuration
+public class SampleJob extends AbstractJobConfig {
+
+    @Bean
+    public Job testJob() {
+        return new JobBuilder("testJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    IntStream.range(0, 10)
+                            .forEach(System.out::println);
+
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}
